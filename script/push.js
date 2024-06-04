2423bbeadc3c5d8f73fd2a0050b72d986c25df27
mmx <1714385126@qq.com>
2024-06-04 11:33:34 +0800

    feat: auto push code

--- script/push.js
+++ script/push.js
@@ -6,11 +6,12 @@ function pushCode() {
   const options = {
     cwd: path.resolve(__dirname, ".."),
     encoding: "utf8",
+    maxBuffer: 0,
   };
 
-  process.execSync("git fetch", options);
+  process.execSync("git pull", options);
   process.execSync("git add .", options);
-  process.execSync("git commit -m 'feat: auto push code'", options);
+  process.execSync(`git commit -m "feat: auto push code"`, options);
   process.execSync("git push", options);
 }
 
